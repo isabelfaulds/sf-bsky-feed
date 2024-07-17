29c5d34330040c66c225a291fa8d49d8d9182792
Lauren F <61340897+manyshapes@users.noreply.github.com>
2024-07-16 18:19:30 -0700

    changed imports in main & made host 0.0.0.0 but can switch back to 127.0.0.1

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,12 @@
-from app import app
+from server.app import app
+import logging
+from server.logger import logger
 
 if __name__ == '__main__':
     # FOR DEBUG PURPOSE ONLY
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    # 127.0.0.1:8000
+    logging.basicConfig(level=logging.DEBUG, 
+                        filename='flask.log', 
+                        filemode='a', 
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    app.run(host='0.0.0.0', port=8000, debug=True)
